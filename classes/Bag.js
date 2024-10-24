3595b794f5404723fd45c9f671dd69b4fa4b0b64
Csilla Toth <csillaout@gmail.com>
2024-10-24 15:42:32 +0100

    bags

--- classes/Bag.js
+++ classes/Bag.js
@@ -1,7 +1,24 @@
-class Bag {
+import Person from ("./Person.js")
 
-
+class Bag extends Person{
+  #owner
+  constructor(weight, id, name, destination, bags) {
+    super(name, destination, bags)
+    this.weight = weight;
+    this.id = id;
+    this.#owner = null;
+  }
+  getOwner(){
+      return this.#owner
+  }
+  assignOwner(person){
+      if(person instanceof Person){
+          this.#owner = person
+          person.addBag(this)
+      }else{
+          throw new Error("owner must ba a valid Person instance")
+      }
+  }
 }
-
 
 module.exports = Bag;
